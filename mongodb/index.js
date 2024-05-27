cca8fef43182f76ea07ee57e7c4e07f9e929e3ef
shreyaww <2020.shreya.singh@ves.ac.in>
2024-05-27 16:01:40 +0530

    Updated Mongoose

--- mongodb/index.js
+++ mongodb/index.js
@@ -13,12 +13,56 @@ async function run() {
     console.log('CONNECTED');
 
     const db = client.db('animals');
+    // CREATE  DATA
+
     // const result = db.collection('mammals').insertOne({
-    //     name: 'dog',
+    //     name: 'doggie',
     //     legs: 4
     // });
     // console.log('INSERTED');
 
+
+    // db.collection('mammals').find().toArray(function(err, result){
+    //     if(err) throw err;
+
+    //     console.log(result);
+    // })
+
+
+
+    // UPDATE DATA
+
+    // db.collection('mammals').findOneAndUpdate({
+    //     _id: new ObjectId('665042324e643b07e9113d72')
+
+    // },
+    //     { $set: { name: 'updated_name' } }
+    // ).then(result => {
+    //     console.log(result)
+    // }).catch(err => {
+    //     console.log(err)
+    // });
+
+
+    // DELETE 
+
+    // db.collection('mammals').deleteMany({
+    //     name : 'horse'
+    // })
+
+    // db.collection('mammals').deleteOne({
+    //     name : 'dog'
+    // })
+
+    // db.collection('mammals').deleteAndFind({
+    //     name : 'dog'
+    // })
+
+    db.collection('mammals').findOneAndDelete({
+        _id: new ObjectId('665042324e643b07e9113d72')
+    }).then(result => {
+        console.log(result)
+    })
 
 }
 
--- mongoose/app.js
+++ mongoose/app.js
@@ -1,9 +1,138 @@
 const mongoose = require('mongoose');
+const User = require('./models/User1');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost:27017/mongoose');
 
 mongoose.connection
-    .once('open', () => console.log('CONNECTED'))
-    .on('error', (err) =>{
-        console.log(`Could not Connect` , err)
+    .once('open', () => console.log('CONNECTED')) // when connection to db is open 
+    .on('error', (err) => {
+        console.log(`Could not Connect`, err)
     })
+
+app.get('/', (req, res) => {
+    res.send('ROOT') // we can also send a complete html webpage using template strings here 
+})
+
+app.post('/users', (req, res) => {
+    // const newUser = User({
+    //     firstName: 'Abhishek',
+    //     lastName: 'Singh',
+    //     isActive: 1
+    // })
+
+    const newUser = User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        isActive: req.body.isActive
+    })
+
+    newUser.save()
+        .then(dataSaved => {
+            console.log('User saved successfully:', dataSaved)
+            res.send("User data saved")
+        })
+        .catch(err => {
+            console.error('Error saving user:', err)
+            res.status(404).send('USER NOT SAVED BECAUSE ', err)
+        });
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.status(200).send(users);
+    })
+})
+
+
+// patch is used when we want to update only one column - firstname in this case
+// app.patch('/users/:id', (req, res) =>{
+//     const id = req.params.id; //extracts the value of the id parameter from the URL and assigns it to the id variable.
+//     const firstName = req.body.firstName; //retrieves the value of the firstName property from the request body 
+
+//     // User.findByIdAndUpdate({_id : id}, { $set : {firstName : firstName}}, {new : true})
+//     //     .then(savedUser =>{
+//     //         res.send("USER UPDATED AND SAVED BY PATCH")
+//     //     })
+
+
+//     //{new: true} option ensures the updated document is returned.
+//     User.findByIdAndUpdate(id, { $set : {firstName : firstName}}, {new : true})
+//     .then(savedUser =>{
+//         res.send("USER UPDATED AND SAVED BY PATCH")
+//     })
+// })
+
+
+// app.put('/users/:id', (req, res) =>{
+//     const id = req.params.id; 
+//     const firstName = req.body.firstName;
+//     const lastName = req.body.lastName;
+
+//     User.findByIdAndUpdate(id, { $set : {firstName : firstName, lastName : lastName}}, {new : true})
+//     .then(savedUser =>{
+//         res.send("USER UPDATED AND SAVED BY PUT")
+//     })
+// })
+
+
+app.put('/users/:id', (req, res) =>{
+    const id = req.params.id; 
+    
+    User.findOne({_id : id})
+    .then(User =>{
+        User.firstName = req.body.firstName;
+        User.lastName = req.body.lastName;
+
+        User.save().then(UserSaved =>{
+            res.send(UserSaved)
+        })
+    })
+})
+
+// app.delete('/users/:id', (req, res) =>{
+//     const id = req.params.id; 
+    
+//     User.findOne({_id : id})
+//     .then(User =>{
+//         User.remove().then(userRemoved =>{
+//             res.send('User Removed' + userRemoved)
+//         });
+//     })
+// })
+
+app.delete('/users/:id', (req, res) =>{
+    const id = req.params.id; 
+    
+    User.findByIdAndDelete({_id : id})
+    .then(UserRemoved =>{
+        res.send(`User ${UserRemoved.firstName} removed`)
+    })
+})
+
+
+
+
+// app.post('/users')
+
+// const newUser = User({
+//     firstName : 'Yashi',
+//     lastName : ' Singh',
+//     isActive : 1
+// })
+
+// newUser.save()
+//   .then(dataSaved => console.log('User saved successfully:', dataSaved))
+//   .catch(err => console.error('Error saving user:', err));
+
+
+const port = 4444 || process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
